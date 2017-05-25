c46b965885332b6af88babffe81cea94899fee17
TestLZX <sbdklzx@gmail.com>
2017-05-25 21:57:47 +0800

    0525
    测试selenium单选框

---
+++ TetsSelenium/src/Demo/TestButton.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestButton {
+	WebDriver driver=new FirefoxDriver();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
